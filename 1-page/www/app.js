4ad26d0dca4cd64e25c162f65458d54c5f08f0da
Jarkko Aalto <jarkko.aalto@live.fi>
2019-07-28 18:07:12 +0300

    project 01 - Adder. type 2 numbers and click add buttom to add these numebers and show result

--- 1-page/www/app.js
+++ 1-page/www/app.js
@@ -8,3 +8,22 @@ function sayHello() {
 function Clear() {
   myname = document.getElementById('myname').value = '';
 }
+
+
+function Add(){
+  var firstnumber = document.getElementById('firstnumber').value;
+  var secondnumber = document.getElementById('secondnumber').value;
+
+  var firstint = parseFloat(firstnumber);
+  var secondint = parseFloat(secondnumber);
+
+  var add = firstint + secondint;
+
+  // ons.notification.alert('Added ' + add);
+  ons.notification.toast('Added ' + add, { timeout: 2000 });
+}
+
+function Clearall() {
+  firstnumber = document.getElementById('firstnumber').value = '';
+  secondnumber = document.getElementById('secondnumber').value = '';
+}
